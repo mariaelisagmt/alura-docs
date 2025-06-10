5253ed3f834262270b5bb2771b4fe07998e63677
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-06-10 08:29:11 -0300

    REFACT: add socket-front

--- src/socket-back.js
+++ src/socket-back.js
@@ -8,4 +8,11 @@ io.on("connection", (socket) => {
         socket.broadcast.emit("texto_editor_clientes", texto);
         console.log(texto)
     });
+
+    socket.on("disconnect", (motivo) => {
+        console.log(`Cliente "${socket.id}" desconectado!
+        Motivo: ${motivo}`);
+    });
 });
+
+
