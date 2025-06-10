3bc5bf2fe18ca9e1f0a222d5b5abf22e38076164
Maria Elisa Gomes de Matos <mariaelisagmt@gmail.com>
2025-06-10 08:15:39 -0300

    FEAT: Emit events

--- src/socket-back.js
+++ src/socket-back.js
@@ -2,4 +2,10 @@ import io from './servidor.js'
 
 io.on("connection", (socket) => {
     console.log("Um cliente se conectou! ID:", socket.id);
+
+    socket.on("texto_editor", (texto) => {
+        //emite para todos, menos para o que está conectado/emitindo
+        socket.broadcast.emit("texto_editor_clientes", texto);
+        console.log(texto)
+    });
 });
